a47ac2e2a942d3b1ec64adc4851549c9bf9b20e1
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2016-11-28 12:56:32 -0500

    Pseudocoded my steps

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+  // Create variables for game
+    // goal score
+    // user score
+    // wins
+    // losses
+    // crystal values array
+
+
+  //Set new and / or game function
+    // Set user score to 0
+    // Set each crystal to a new value at random with jQuery attr
+    // Pick number between 19 and 120 at random for goal score
+
+
+  // Set click events for crystals
+    // each one adds a value amount to user score
+    // win game if user score = goal score
+    // lose game if user score > goal score
